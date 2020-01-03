127e4d97aa913bad8acbaa7e91c4db15ac8b903d
yoselinvera <isc_vera.y@tesvb.edu.mx>
2020-01-02 20:16:27 -0600

    Cambio de direcciones y pacman

--- resources/views/figuras.blade.php
+++ resources/views/figuras.blade.php
@@ -20,6 +20,8 @@ Salto adelante: Shift derecho -->
     <script>
         bonsai.run(document.getElementById('bonsai'), {
             code: function() {
+                var eye2 = new Circle(5,30,7)
+                    .attr('fillColor', '#fff');
                 var eye = new Circle(10,-35,7)
                     .attr('fillColor', '#fff');
                 var on  = 'l 60 0 L 60 0 A 60 60 0 1 1 48.54 -35.26';
@@ -36,38 +38,65 @@ Salto adelante: Shift derecho -->
                     pm.path(i%2 ? on : off);
                     i++;
                 })();
-                stage.on('keydown', function(e) {
 
+                stage.on('keydown', function(e) {
                     console.log(e.keyCode)
                     console.log("ADELANTE");
-                    if (e.keyCode==39){
+                    if (e.keyCode===39){
+                        eye .attr('fillColor', '#fff');
+                        var on  = 'l 60 0 L 60 0 A 60 60 0 1 1 48.54 -35.26';
+                        var off = 'l 60 0 L 60 0 A 60 60 0 1 1 60 -0.0001';
+                        var pm = new bonsai.Path().attr('fillColor', 'red');
+
                         pacman.attr({
+                            'y': 100, 'rotation': 0.1,
                             x: pacman.attr('x')+2
-                        })
+                        }).addTo(stage);
+                        pacman.addChild.call(pacman, [pm, eye]);
+                        (function loop(e) {})();
+
                     }
-                    else if (e.keyCode==37){
+                    else if (e.keyCode===37){
                         console.log("ATRAS");
+                        eye.attr('fillColor', '#fff');
+                        var on  = 'l 60 0 L 60 0 A 60 60 0 1 1 48.54 -35.26';
+                        var off = 'l 60 0 L 60 0 A 60 60 0 1 1 60 -0.0001';
+                        var pm = new bonsai.Path().attr('fillColor', 'red');
+
                         pacman.attr({
+                            'y': 100, 'rotation': 3,
                             x: pacman.attr('x')-2
-                        })
+                        }).addTo(stage);
+                        pacman.addChild.call(pacman, [pm, eye]);
+                        (function loop(e) {})();
                     }
-                    else if(e.keyCode==38){
+                    else if(e.keyCode===38){
                         console.log("ARRIBA");
-                        pacman.animate('0.1s', {
+                        eye .attr('fillColor', '#fff');
+                        var on  = 'l 60 0 L 60 0 A 60 60 0 1 1 48.54 -35.26';
+                        var off = 'l 60 0 L 60 0 A 60 60 0 1 1 60 -0.0001';
+                        var pm = new bonsai.Path().attr('fillColor', 'red');
+
+                        pacman.attr({
+                            'y': 100, 'rotation': 5,
                             y: pacman.attr('y')-2
-                        }, {
-                            onEnd: function() {
-                                pacman.animate('0.1s', {
-                                    y: pacman.attr('y')+2
-                                })
-                            }
-                        });
+                        }).addTo(stage);
+                        pacman.addChild.call(pacman, [pm, eye]);
+                        (function loop(e) {})();
                     }
                     else {
-                        console.log("HHHH");
+                        console.log("ABAJO");
+                        eye .attr('fillColor', '#fff');
+                        var on  = 'l 60 0 L 60 0 A 60 60 0 1 1 48.54 -35.26';
+                        var off = 'l 60 0 L 60 0 A 60 60 0 1 1 60 -0.0001';
+                        var pm = new bonsai.Path().attr('fillColor', 'red');
+
                         pacman.attr({
+                            'y': 100, 'rotation': 2,
                             y: pacman.attr('y')+2
-                        })
+                        }).addTo(stage);
+                        pacman.addChild.call(pacman, [pm, eye]);
+                        (function loop(e) {})();
                     }
                 })
             },
